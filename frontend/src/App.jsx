e4c525d59ba75f80377fdbd804eb45fb2b7d2f48
pqkhai2401 <pqkhai2401@gmail.com>
2026-01-06 10:37:10 +0700

    add pages/cart, pages/checkout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,21 +6,23 @@ import Shop from "./pages/Shop.jsx";
 import NotFound from "./pages/404.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
 import Home from "./pages/Home.jsx";
-
+import Cart from "./pages/Cart.jsx";
+import Checkout from "./pages/Checkout.jsx";
 
 function App() {
-
-	return (
-	<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<MainLayout />}>
-						<Route index element={<Home />} />
-						<Route path="/shop" element={<Shop />} />
-						<Route path="*" element={<NotFound />} />
-					</Route>
-				</Routes>
-	</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
