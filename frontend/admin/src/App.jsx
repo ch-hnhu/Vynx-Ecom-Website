f143cb4d9983ccf1f5d8c970d9a8f615af9da7c7
Chung Hue Nhu <0306231416@caothang.edu.vn>
2026-01-09 09:17:08 -0800

    Thêm giao diện trang đăng ký và đăng nhập

--- frontend/admin/src/App.jsx
+++ frontend/admin/src/App.jsx
@@ -2,10 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import DashboardPage from "./pages/DashboardPage";
 import Table from "./pages/Table";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 export default function App() {
 	return (
 		<Routes>
+			<Route path='login' element={<Login />} />
+			<Route path='signup' element={<Signup />} />
 			<Route path='/' element={<MainLayout />}>
 				<Route index element={<DashboardPage />} />
 				<Route path='table' element={<Table />} />
